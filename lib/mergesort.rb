0a39807b1d97be5b2b4d4eb8134ea7558cccd9d6
Tim Heuett <tim.heuett@gmail.com>
2015-04-02 15:34:03 -0700

    add first implementation of mergesort

---
+++ lib/mergesort.rb
@@ -0,0 +1,29 @@
+class MergeSort
+  def self.sort(numbers_array)
+    # start with an array of arrays
+    array = numbers_array.map{ |n| [n] }
+    # loop through and merge pairs until there is only one array left
+    while array.size > 1
+      new_array = []
+      array.each_slice(2) do |set1, set2|
+        new_array << (set2 ? merge(set1, set2) : set1)
+      end
+      # the next loop should iterate over the result of this loop
+      array = new_array
+    end
+    # return the resulting array
+    array.first
+  end
+
+  private
+
+  def self.merge(set1, set2)
+    result = []
+    # until one of the sets runs out, add the lower of the two first elements from each set to the return array
+    until set1.empty? || set2.empty?
+      result << (set1.first <= set2.first ? set1.shift : set2.shift)
+    end
+    # dump the remaining elements into the return array
+    result += set1 + set2
+  end
+end
