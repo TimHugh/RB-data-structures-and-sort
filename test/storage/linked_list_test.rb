c6adfe754ce2c6e86e5c1bfc875c2de791a5ea02
Tim Heuett <tim.heuett@gmail.com>
2015-04-06 15:54:10 -0700

    add test descriptions for linked_list

---
+++ test/storage/linked_list_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+require 'linked_list'
+
+describe LinkedList do
+  before do
+    list = LinkedList.new
+    list.add(2)
+    list.add(4)
+    list.add(7)
+  end
+
+  it 'stores new values at the front' do
+  end
+
+  it 'returns the correct node when a value is searched' do
+  end
+
+  it 'returns null when searching for a value that is not present' do
+  end
+
+  it 'correctly removes values' do
+  end
+
+  it 'returns a value when it removes it from the list' do
+  end
+
+  it 'returns null when removing a value that is not present' do
+  end
+
+  it 'creates a comma-separated string when the to_s function is called' do
+  end
+end
