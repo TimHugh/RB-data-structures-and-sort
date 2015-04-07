20ce0379429d172dfcf04e1c17189a5332e10c3f
Tim Heuett <tim.heuett@gmail.com>
2015-04-07 16:30:31 -0700

    add test for element ordering

--- test/storage/stack_test.rb
+++ test/storage/stack_test.rb
@@ -15,4 +15,13 @@ describe Stack do
     @stack.pop
     @stack.size.must_equal 0
   end
+
+  it 'retrieves objects FIFO' do
+    @stack.push 1
+    @stack.push 2
+    @stack.push 3
+    @stack.pop.must_equal 3
+    @stack.pop.must_equal 2
+    @stack.pop.must_equal 1
+  end
 end
