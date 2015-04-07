a8f15c9e980ac7bd6132f0a6dc4d01dd372a5ea2
Tim Heuett <tim.heuett@gmail.com>
2015-04-07 16:23:58 -0700

    add stack class with push and pop methods

---
+++ lib/stack.rb
@@ -0,0 +1,22 @@
+class Stack
+  attr_accessor :head, :size
+
+  Node = Struct.new(:value, :next)
+
+  def initialize
+    @size = 0
+  end
+
+  def push(value)
+    node = Node.new(value, @head)
+    @head = node unless @head
+    @size += 1
+  end
+
+  def pop
+    node = @head
+    @head = @head.next
+    @size -= 1
+    node.value
+  end
+end
