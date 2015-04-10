f8f3972e11e67929d79d3c2c9e7f212b709a0479
Tim Heuett <tim.heuett@gmail.com>
2015-04-09 20:15:50 -0700

    add store/recall test

--- test/storage/hash_table_test.rb
+++ test/storage/hash_table_test.rb
@@ -9,4 +9,9 @@ describe HashTable do
   it 'is created empty' do
     @hashtable.size.must_equal 0
   end
+
+  it 'can store and recall values' do
+    @hashtable[:one] = "test value"
+    @hashtable[:one].must_equal "test value"
+  end
 end
