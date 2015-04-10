66ae50a1630918127d73976963b82250aa342e8f
Tim Heuett <tim.heuett@gmail.com>
2015-04-09 20:44:51 -0700

    add []= method to add/remove values from table

--- lib/hash_table.rb
+++ lib/hash_table.rb
@@ -1,11 +1,40 @@
 class HashTable
   attr_reader :size
+  Node = Struct.new(:key, :value, :next, :length)
 
   def initialize
     @size = 0
+    @maxSize = 10
+    @table = []
+  end
+
+  def []=(key, value)
+    if value.nil?
+      remove(key)
+    else
+      add(key, value)
+    end
   end
 
   protected
+
+  def add(key, value)
+    node = Node.new(key, value)
+    hash = keyToHash(key)
+
+    node.next = @table[hash]
+    node.length = @table[hash].nil? ? 1 : @table[hash].length + 1
+
+    @table[hash] = node
+    @size += 1
+  end
+
+  def remove(key)
+  end
+
+  def keyToHash(key)
+    hashValue(key) % @maxSize
+  end
 
   def hashValue(key)
     key.inspect.split('').inject(0) { |memo, char| memo += char.ord }
