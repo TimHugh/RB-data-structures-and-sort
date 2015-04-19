cd48ea00874e646e6557aa819b2835b887172cd9
Tim Heuett <tim.heuett@gmail.com>
2015-04-19 12:40:54 -0700

    add to_a, to_s methods with inorder traversal

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -1,11 +1,41 @@
 class BinaryTree
   attr_accessor :root
 
+  class NullNode
+    def to_a(order)
+      return []
+    end
+
+    def nil?
+      true
+    end
+  end
+
   class Node
     attr_accessor :value, :left, :right
 
     def initialize(value)
       @value = value
+      @left = @right = NullNode.new
+    end
+
+    def add(*values)
+      values.each do |val|
+        if @left.nil?
+          @left = Node.new(val)
+        elsif @right.nil?
+          @right = Node.new(val)
+        else
+          raise "Node already has two children"
+        end
+      end
+    end
+
+    def to_a(order)
+      case order
+      when :inorder
+        return @left.to_a(order) + [@value] + @right.to_a(order)
+      end
     end
   end
 
@@ -13,5 +43,13 @@ class BinaryTree
     return nil if cur_node.nil?
     return cur_node if cur_node.value == value
     return find(value, cur_node.left) || find(value, cur_node.right)
+  end
+
+  def to_s(order = :inorder)
+    to_a(order).join(', ')
+  end
+
+  def to_a(order = :inorder)
+    @root.to_a(order)
   end
 end
