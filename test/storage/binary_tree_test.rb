150e074081a2b45be94605d4f126b9ca074127cb
Tim Heuett <tim.heuett@gmail.com>
2015-04-19 12:10:18 -0700

    add search test

--- test/storage/binary_tree_test.rb
+++ test/storage/binary_tree_test.rb
@@ -10,4 +10,10 @@ describe BinaryTree do
     @tree.root = BinaryTree::Node.new('Root')
     @tree.root.value.must_equal 'Root'
   end
+
+  it 'returns a node when searching for a value' do
+    @tree.find('Root').must_be_nil
+    @tree.root = BinaryTree::Node.new('Root')
+    @tree.find('Root').value.must_equal 'Root'
+  end
 end
