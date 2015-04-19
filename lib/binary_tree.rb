149c78e7e6b2abdc301d32ed5bf5123b5809d8e0
Tim Heuett <tim.heuett@gmail.com>
2015-04-19 12:10:33 -0700

    add find method

--- lib/binary_tree.rb
+++ lib/binary_tree.rb
@@ -1,5 +1,17 @@
 class BinaryTree
   attr_accessor :root
 
-  Node = Struct.new(:value, :left, :right)
+  class Node
+    attr_accessor :value, :left, :right
+
+    def initialize(value)
+      @value = value
+    end
+  end
+
+  def find(value, cur_node = @root)
+    return nil if cur_node.nil?
+    return cur_node if cur_node.value == value
+    return find(value, cur_node.left) || find(value, cur_node.right)
+  end
 end
